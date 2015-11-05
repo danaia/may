60da406eb020314cc272f70e13b685fe10b1a456
Dana <martinelli@gmail.com>
2015-11-05 02:24:00 -0500

    adding the body

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,20 +4,29 @@
 
 
 @section('content')
-<div class="container-fluid"></div>
-<div class="row">
-	<div class="col-md-12">
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#">
-						<span class="logo"><strong>may</strong>/press</span><span class="tag">Independent Online Publisher</span>
-					</a>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<nav class="navbar navbar-default navbar-fixed-top ">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#">
+							<span class="logo"><span class="may">may</span>/press</span><span class="tag">Independent Online Publisher</span>
+						</a>
+					</div>
 				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<img src="" alt="">
+			<div class="writing">
+				<h1>The Machine</h1>
+				<p>
+					The machine woke one day. It was fall. Late 2015.
+				</p>
 			</div>
-		</nav>
-		<div class="panel">
-			<button class="btn btn-primary">Nice</button>
 		</div>
 	</div>
 </div>
