dce8f456ba63950528f818197130014c58ac1de0
Dana <martinelli@gmail.com>
2015-11-05 01:52:39 -0500

    adds the logo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,5 +4,21 @@
 
 
 @section('content')
-    <button>nice</button>
+<div class="container-fluid"></div>
+<div class="row">
+	<div class="col-md-12">
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">
+						<span class="logo"><strong>may</strong>/press</span><span class="tag">Independent Online Publisher</span>
+					</a>
+				</div>
+			</div>
+		</nav>
+		<div class="panel">
+			<button class="btn btn-primary">Nice</button>
+		</div>
+	</div>
+</div>
 @stop
