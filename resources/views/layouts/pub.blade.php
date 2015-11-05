3c4f7691f0b58b8351bddfba15bc7e3937941f3a
Dana <dana@hotmetal.in>
2015-11-05 03:26:50 +0000

    creates the public layout and adds css

---
+++ resources/views/layouts/pub.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
