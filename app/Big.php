4734670d3c11ba600f4fac4570c054b2e87b4b9c
Dana <dana@hotmetal.in>
2015-11-05 02:10:05 +0000

    adds snippets and save actions

---
+++ app/Big.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Big extends Model
+{
+     protected $fillable = ['bigText'];
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,3 +8,7 @@ Route::get('/may', function () {
     return 'nice may to winter'; 
 });
 
+Route::get('go', 'BigController@index')->name('go');
+
+
+Route::get('big/{id}', 'BigController@big');
