773ceb93ec48b5e818a2f8b1eff5e814373b3a4b
Dana <martinelli@gmail.com>
2015-11-06 20:38:17 -0500

    format text areas

--- resources/views/layouts/pub.blade.php
+++ resources/views/layouts/pub.blade.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title>May Press</title>
+        <title>May</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/app.css">
     </head>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,27 +7,15 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<nav class="navbar navbar-default navbar-fixed-top ">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<a class="navbar-brand" href="#">
-							<span class="logo"><span class="may">may</span>/press</span><span class="tag">An Online Publisher</span>
-						</a>
-					</div>
-				</div>
-			</nav>
+			<div class="may">m˚p</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12">
-			<img src="" alt="">
 			<div class="writing">
-				<div class="sectionDiv" style="background-color: #F9E1B1; height: 10px">
-					
-				</div>
+				<div class="chapter">Chapter 1</div>
 				<h1>The Machine</h1>
-				<p>
-					The machine woke one day. It was fall. Late 2015.
+				<p class="write" contenteditable="true" placeholder='Begin your journey here...'>
 				</p>
 			</div>
 		</div>
